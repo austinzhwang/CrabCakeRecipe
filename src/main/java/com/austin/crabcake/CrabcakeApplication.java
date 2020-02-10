ed49eea862d355666f74e04222212dd92732fe79
GBTC440019UR <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-10 10:34:48 -0500

    first commit

---
+++ src/main/java/com/austin/crabcake/CrabcakeApplication.java
@@ -0,0 +1,13 @@
+package com.austin.crabcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabcakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrabcakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/austin/crabcake/CrabcakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austin.crabcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabcakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
